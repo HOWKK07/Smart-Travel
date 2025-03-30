b295d7364f32ddc98c172de8aaaa8d5f22ecc579
quachnhuthong <quachthong10a5@gmail.com>
2025-03-30 18:26:03 +0700

    thêm form đăng ký tour

--- SmartTravel/SmartTravel.GUI/DangKyTour.cs
+++ SmartTravel/SmartTravel.GUI/DangKyTour.cs
@@ -16,5 +16,55 @@ namespace SmartTravel.GUI
         {
             InitializeComponent();
         }
+
+        private void DangKyTour_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }

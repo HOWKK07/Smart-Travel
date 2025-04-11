8a28b0f2f9862ebd5bbe861af2ef93c070b6846f
hGiangg04 <giangprohynam113@gmail.com>
2025-04-11 12:18:44 +0700

    feat: cập nhật form quản lý điểm tham quan, thêm resource và chỉnh sửa giao diện

--- SmartTravel/SmartTravel.GUI/QLDiaDiemDiemThamQuan.cs
+++ SmartTravel/SmartTravel.GUI/QLDiaDiemDiemThamQuan.cs
@@ -16,5 +16,10 @@ namespace SmartTravel.GUI
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }

f502d0704174c3f0ce80aa13614617b9320ae2c9
quachnhuthong <quachthong10a5@gmail.com>
2025-04-11 17:08:10 +0700

    Viết unit test cho chức năng đánh giá địa điểm

--- SmartTravel/SmartTravel.GUI/Main.Designer.cs
+++ SmartTravel/SmartTravel.GUI/Main.Designer.cs
@@ -44,6 +44,7 @@ namespace SmartTravel.GUI
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -213,11 +214,21 @@ namespace SmartTravel.GUI
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(305, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "label1";
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -279,5 +290,6 @@ namespace SmartTravel.GUI
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
     }
 }

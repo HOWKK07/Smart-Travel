f49e395da8a5a0616c44bd8d285fc66f7e9a2c5d
quachnhuthong <quachthong10a5@gmail.com>
2025-04-11 17:22:46 +0700

    Code class DatTourTronGoi gồm các thuộc tính như thiết kế

--- SmartTravel/SmartTravel.GUI/Main.Designer.cs
+++ SmartTravel/SmartTravel.GUI/Main.Designer.cs
@@ -46,6 +46,7 @@ namespace SmartTravel.GUI
             this.button6 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -233,11 +234,21 @@ namespace SmartTravel.GUI
             this.label2.TabIndex = 19;
             this.label2.Text = "label2";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(217, 335);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "label3";
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button6);
@@ -303,5 +314,6 @@ namespace SmartTravel.GUI
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
